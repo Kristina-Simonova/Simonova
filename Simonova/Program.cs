8ae5fd20ae876f3284e3c206dcdb7e7e1b08b5ab
Kristina-Simonova <kristmassimonova@yandex.ru>
2025-04-24 12:04:42 +0300

    Индивидульное задание

--- Simonova/Program.cs
+++ Simonova/Program.cs
@@ -10,7 +10,13 @@ namespace Simonova
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Для значения х = {x}, F = {F}.");
+            Console.Write("Введите значение парамаетра х = ");
+            string s = Console.ReadLine();
+            double x = Convert.ToDouble(s);
+            double F = 3 * Math.Sin(x + 2);
+            Console.WriteLine("3Sin(x + 2) = " + F);
+
+            Console.ReadKey();
         }
     }
 }
